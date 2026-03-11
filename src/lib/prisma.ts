b5ee6050255d72211aadff0537b5903379ec5837
kibuukahassan12-code <kibuukahassan12@gmail.com>
2026-03-11 12:47:22 +0300

    Remove broken PrismaBetterSQLite3 adapter and restore stable Prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,9 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
-import Database from 'better-sqlite3';
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-const sqliteDb = new Database('./dev.db');
-const adapter = new PrismaBetterSQLite3(sqliteDb);
+export const prisma = global.prisma || new PrismaClient()
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma
