efea17942cdf2161a9de5e60247def24521f0b7d
kibuukahassan12-code <kibuukahassan12@gmail.com>
2026-03-12 01:38:42 +0300

    Initial commit

---
+++ lib/prisma.ts
@@ -0,0 +1,1 @@
+import { PrismaClient } from '@prisma/client'; let prisma; if (process.env.NODE_ENV === 'production') { prisma = new PrismaClient({ engine: 'binary' }); } else { if (!global.prisma) { global.prisma = new PrismaClient({ engine: 'binary' }); } prisma = global.prisma; } export default prisma;
